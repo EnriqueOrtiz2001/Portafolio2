7b704d33eafc6bd330955331c9ef00ed399aa861
EnriqueOrtiz2001 <enriquotero@gmail.com>
2025-01-13 12:16:23 -0500

    FINAL

--- src/app/inicio/page.tsx
+++ src/app/inicio/page.tsx
@@ -70,7 +70,7 @@ const Inicio = () => {
     <div className="texto-sobre-mi">
      
       <p className="saludo">
-        ¡Hola! Soy <strong>ENRIQUE ORTIZ</strong>, tengo <strong>23 AÑOS</strong> y soy estudiante de la 
+        ¡Hola! Soy <strong>ENRIQUE ORTIZ</strong>, tengo <strong>23 AÑOS</strong> y estudié de la 
         <strong> UNIVERSIDAD TÉCNICA DE AMBATO</strong>. <br />
         Soy un Ingeniero en Software con experiencia como programador full-stack junior, especializado en el desarrollo de aplicaciones web y móviles.  Siempre estoy motivado para aprender, crecer profesionalmente y contribuir al éxito de proyectos innovadores. 🚀<br />
         Me gusta el ajedrez♟️, escuchar música 🎵 y hacer Stream🔴
